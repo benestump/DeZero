6dd71f594846087fc459ee0a14877419a3500aba
benestump <6yeargeun@gmail.com>
2020-12-20 17:09:59 +0900

    Improve function

--- dezero/core.py
+++ dezero/core.py
@@ -1,5 +1,13 @@
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 class Variable:
-    def __init__(self, data:ndarray):
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} is not supported.')
+
         self.data = data
         self.grad = None
         self.creator = None
@@ -8,42 +16,48 @@ class Variable:
         self.creator = func
 
     def backward(self):
-        f = self.creator # get function
-        if f is not None:
-            x = f.input # get function's input
-            x.grad = f.backward(self.grad) # call functions's backward
-            x.backward()
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop() # get function
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creatro)
 
 class Function:
     def __call__(self, input:Variable) -> Variable:
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
         self.input = input
         self.output = output
         return output
 
-    def forward(self, x:ndarray):
+    def forward(self, x):
         raise NotImplementedError()
 
-    def backward(self, gy:ndarray):
+    def backward(self, gy):
         raise NotImplementedError()
 
 class Square(Function):
-    def forward(self, x:ndarray) -> ndarray:
+    def forward(self, x) :
         return x ** 2
 
-    def backward(self, gy:ndarray) -> ndarray:
+    def backward(self, gy):
         x = self.input.data
         gx = 2 * x * gy
         return gx
 
 class Exp(Function):
-    def forward(self, x:ndarray) -> Variable:
+    def forward(self, x) -> Variable:
         return np.exp(x)
 
-    def backward(self, gy:ndarray) -> ndarray:
+    def backward(self, gy):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
@@ -55,4 +69,11 @@ def neumerical_diff(f:Function, x:Variable, eps=1e-4):
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
 
