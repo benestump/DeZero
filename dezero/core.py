0a053904ed42fcc75f09a4fac9af008f0696eccb
benestump <6yeargeun@gmail.com>
2020-12-30 22:01:35 +0900

    STEP 22
    Add __neg__, __sub__, __rsub__, __truediv__, __rtruediv__, __pow__

--- dezero/core.py
+++ dezero/core.py
@@ -61,6 +61,24 @@ class Variable:
     def __radd__(self, other):
         return add(self, other)
 
+    def __neg__(self):
+        return neg(self)
+
+    def __sub__(self, other):
+        return sub(self, other)
+
+    def __rsub__(self, other):
+        return rsub(self, other)
+
+    def __truediv__(self, other):
+        return div(self, other)
+
+    def __rtruediv__(self, other):
+        return rdiv(self, other)
+
+    def __pow__(self, other):
+        return pow(self, other)
+
     @property
     def shape(self):
         return self.data.shape
@@ -149,6 +167,13 @@ class Add(Function):
     def backward(self, gy):
         return gy, gy
 
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(selfself, gy):
+        return gy, -gy
 
 class Mul(Function):
     def forward(self, x0, x1):
@@ -163,6 +188,32 @@ class Div(Function):
     def forward(self, x0, x1):
         y = x0 / x1
         return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c - 1) * gy
+        return gx
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
 
 class Square(Function):
     def forward(self, x) :
@@ -203,3 +254,29 @@ def add(x0, x1):
 def mul(x0, x1):
     x1 = as_array(x1)
     return Mul()(x0, x1)
+
+def neg(x):
+    return Neg()(x)
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+def pow(x, c):
+    return Pow(c)(x)
+
+x = Variable(np.array(2.0))
+y = x ** 3
+print(y)
